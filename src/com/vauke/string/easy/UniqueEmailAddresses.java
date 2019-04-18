b6ba94af04852c09e397c38e6ab83c502cc6beca
Vauke <VaukeH@gmail.com>
2019-04-18 15:50:11 +0800

    build: init repo

---
+++ src/com/vauke/string/easy/UniqueEmailAddresses.java
@@ -0,0 +1,16 @@
+package com.vauke.string.easy
+
+/**
+ * No.929
+ * Difficulty : Easy
+ * Title      : Unique Email Addresses
+ * url        : https://leetcode.com/problems/unique-email-addresses/
+ *
+ * Created by Vauke on 2019-04-18 15:26
+ */
+
+public class UniqueEmailAddresses {
+    public int numUniqueEmails(String[] emails) {
+
+    }
+}
