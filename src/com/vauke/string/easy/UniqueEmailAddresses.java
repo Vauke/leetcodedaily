50c8e13dab1fd27d70e4d2d62b9feb0babe557f9
Vauke <VaukeH@gmail.com>
2019-04-18 17:42:14 +0800

    refactor(add 804 & 929):

--- src/com/vauke/string/easy/UniqueEmailAddresses.java
+++ src/com/vauke/string/easy/UniqueEmailAddresses.java
@@ -4,13 +4,45 @@ package com.vauke.string.easy
  * No.929
  * Difficulty : Easy
  * Title      : Unique Email Addresses
- * url        : https://leetcode.com/problems/unique-email-addresses/
+ * url        : https://leetcode-cn.com/problems/unique-email-addresses/
  *
  * Created by Vauke on 2019-04-18 15:26
  */
-
 public class UniqueEmailAddresses {
     public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        String local = "";
+        String domain = "";
+        String[] match = {};
+        int count = 0;
 
+        for (int i = 0; i < emails.length; i++) {
+            match = emails[i].split("@");
+
+            if (emails[i].length() >= 1 && emails[i].length() <= 100) {
+                local = match[0];
+                domain = match[1];
+
+                /* find first index of plus sign('+'), then substring */
+                int firstIndex = local.indexOf("+");
+                if (firstIndex > 0) {
+                    local = local.substring(0, firstIndex);
+                }
+                
+                /* remove periods('.') */
+                local = local.replace(".", "");
+
+                if (!set.add(local)) {   
+                    if (set.add(domain)) {
+                        count += 1;
+                    }
+                } else {
+                    count += 1;
+                    set.add(domain);
+                }
+            }
+        }
+
+        return count;
     }
 }
---
+++ src/com/vauke/string/easy/UniqueMoseCodeWords.java
@@ -0,0 +1,41 @@
+package com.vauke.string.easy
+
+/**
+ * No.804
+ * Difficulty : Easy
+ * Title      : Unique Morse Code Words
+ * url        : https://leetcode-cn.com/problems/unique-morse-code-words
+ *
+ * Created by Vauke on 2019-04-18 16:05
+ */
+public class UniqueMoseCodeWords {
+    /* 26/0.75 = 35 */
+    private static Map<Character, String> mapper = new HashMap<>(35);
+
+    static {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        for (int i = 0; i < 26; i++) {
+            mapper.put((char)(i + 97), morse[i]);
+        }
+    }
+
+    public int uniqueMorseRepresentations(String[] words) {
+        StringBuffer single = new StringBuffer();
+        Set<String> result = new HashSet<>();
+
+        for (int i = 0, len = words.length, wLen = 0; i < len; i++) {
+            String word = words[i];
+            wLen = word.length();
+
+            for (int j = 0; j < wLen;j++){
+                single.append(mapper.get(word.charAt(j)));
+            }
+
+            result.add(single.toString());
+            single.delete(0, single.length());
+        }
+
+        return result.size();
+    }
+}
