f5ab0d5505023717a6e8a46c639db836a5a47ec9
Vauke <VaukeH@gmail.com>
2019-04-21 10:55:21 +0800

    refactor: refactor CountAndSay.java

--- src/com/vauke/string/easy/CountAndSay.java
+++ src/com/vauke/string/easy/CountAndSay.java
@@ -9,7 +9,7 @@ package com.vauke.string.easy;
  * Created by Vauke on 2019-04-20 22:03
  */
 public class CountAndSay {
-    public String countAndSay(int n) {
+    public String countAndSay1(int n) {
         if (n < 1 || n > 30) return "";
         return doCount("1", n);
     }
@@ -36,5 +36,34 @@ public class CountAndSay {
         }
 
         return doCount(builder.toString(), --n);
+    }
+
+    private StringBuilder builder = new StringBuilder("1");
+
+    public String countAndSay(int n) {
+        if (n < 1 || n > 30) return "";
+
+        if (n <= 1) return builder.toString();
+
+        char[] c = builder.toString().toCharArray();
+
+        int length = c.length;
+
+        builder = new StringBuilder();
+
+        for (int i = 0, j = 0, count = 0; i < length; i = j) {
+            count = 1;
+            char temp = c[i];
+            j = i + 1;
+
+            while (j < length && c[j] == temp) {
+                count++;
+                j++;
+            }
+
+            builder.append("" + count + c[i]);
+        }
+
+        return countAndSay(--n);
     }
 }
