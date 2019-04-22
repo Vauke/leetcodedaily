f6d7b845fc82d6280701f359e4f60d33ef38a65c
Vauke <VaukeH@gmail.com>
2019-04-22 22:19:14 +0800

    refactor: add another way to RomanToInteger.java

--- src/com/vauke/string/easy/RomanToInteger.java
+++ src/com/vauke/string/easy/RomanToInteger.java
@@ -45,4 +45,39 @@ public class RomanToInteger {
 
         return ans;
     }
+
+    // another way
+    public int romanToInt1(String s) {
+        int result = 0;
+        
+        Map<String, Integer> sin = new HashMap<String, Integer>() {{
+            put("I",1);
+            put("V",5);
+            put("X",10);
+            put("L",50);
+            put("C",100);
+            put("D",500);
+            put("M",1000);
+        }};
+        
+        Map<String, Integer> dou = new HashMap<String, Integer>() {{
+            put("IV",4);
+            put("IX",9);
+            put("XL",40);
+            put("XC",90);
+            put("CD",400);
+            put("CM",900);
+        }};
+
+        for(int i = 0, len = s.length() - 1; i < len; i++){
+            result += sin.get(String.valueOf(s.charAt(i)));
+            
+            if (dou.containsKey(s.substring(i, i + 2))) {
+                result -= sin.get(String.valueOf(s.charAt(i))) * 2;
+            }
+        }
+
+        result += sin.get(String.valueOf(s.charAt(s.length() - 1)));
+        return result;
+    }
 }
