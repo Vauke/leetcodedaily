7287cf2e6b83b578288e9e807593c7ac354cd6ca
Vauke <VaukeH@gmail.com>
2019-05-01 21:40:43 +0800

    finish ImplementStrStr.java

--- src/com/vauke/string/easy/ImplementStrStr.java
+++ src/com/vauke/string/easy/ImplementStrStr.java
@@ -13,19 +13,31 @@ public class ImplementStrStr {
         int nLen = needle.length();
         if (needle == null || nLen == 0) return 0;
 
-        int hLen  = needle.length();
+        int hLen  = haystack.length();
+        if (nLen > hLen) return -1;
+
+        if (nLen == 1) return haystack.indexOf(needle.charAt(0));
+
         int index = -1;
 
-        for (int i = 0; i < hLen; i++) {
-            char hChar = haystack.charAt(i);
-            for (int j = index + 1; j < nLen; j++) {
-                char nChar = needle.charAt(j);
+        for (int i = 0, j = 0; i < nLen && j < hLen; i++, j++) {
+            char nChar = needle.charAt(i);
+            char hChar = haystack.charAt(j);
 
-                if (nChar == hChar) {
-                    index = j;
-                    break;
-                }
+            index = j;
+            while (nChar == hChar) {
+                if (i < nLen - 1 && j < hLen - 1) {
+                    nChar = needle.charAt(++i);
+                    hChar = haystack.charAt(++j);
+
+                    if (nChar == hChar && i == nLen - 1) return index;
+                } else break;
             }
+
+            i = -1;
+            j = index;
         }
+
+        return -1;
     }
 }
